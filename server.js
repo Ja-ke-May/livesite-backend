670318b8ff295457ee2f8666ab378e61aa359b54
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2024-08-21 00:38:52 +0100

    origin settings

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'https://livesite-backend.onrender.com',
+  origin: 'https://localhost:3000',
   credentials: true,
 }));
 app.use(helmet());
