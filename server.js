dcedc3fd2395b1209b55095f0dd34655446ff4fc
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2024-08-21 19:10:42 +0100

    rate limit and currentStreamer null

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(bodyParser.json({ limit: '10mb' }));
 // Rate Limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 1000, // limit each IP to 1000 requests per windowMs
+  max: 10000, // limit each IP to 10000 requests per windowMs
 });
 app.use(limiter);
 
