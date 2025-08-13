b44e011012d1d62888fde6da45d4569a853ada55
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2025-08-13 04:07:52 +0100

    xsolla

--- routes/xsolla.js
+++ routes/xsolla.js
@@ -75,9 +75,9 @@ router.post('/get-token', async (req, res) => {
     id: { value: username }
   },
   purchase: {
-    virtual_items: [
-      { sku, quantity: 1 }
-    ]
+    virtual_items: {
+      [String(skuMap[sku].item_id)]: 1  
+    }
   }
 };
 
