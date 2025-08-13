3bcb73505754a0150c21ff1cd7f8bd56c8f55cb2
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2025-08-13 01:18:18 +0100

    xsolla

--- routes/xsolla.js
+++ routes/xsolla.js
@@ -19,10 +19,10 @@ const skuMap = {
   tokens_10000: { item_id: 1055106, amount: 99.99, tokens: 10000 },
 };
 
-// Extract item_id strings for AJV schema keys
-const itemIdKeys = Object.values(skuMap).map(item => item.item_id.toString());
+// Extract SKU keys for AJV schema keys
+const skuKeys = Object.keys(skuMap);
 
-// AJV schema validating only allowed item ID keys in virtual_items
+// AJV schema validating only allowed SKU keys in virtual_items
 const payloadSchema = {
   type: 'object',
   properties: {
@@ -48,8 +48,8 @@ const payloadSchema = {
           type: 'object',
           minProperties: 1,
           additionalProperties: false,
-          properties: itemIdKeys.reduce((acc, id) => {
-            acc[id] = { type: 'integer', minimum: 1 };
+          properties: skuKeys.reduce((acc, sku) => {
+            acc[sku] = { type: 'integer', minimum: 1 };
             return acc;
           }, {}),
         },
@@ -86,16 +86,14 @@ router.post('/get-token', async (req, res) => {
     });
   }
 
-  // Use item_id string as key for virtual_items
-  const itemId = skuMap[sku].item_id.toString();
-
+  // Use SKU string as key for virtual_items (per Xsolla docs)
   const payload = {
     user: {
       id: { value: username },
     },
     purchase: {
       virtual_items: {
-        [itemId]: 1,
+        [sku]: 1,
       },
     },
   };
