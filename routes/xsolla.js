e638f19077fb5812742615a45c5e40064e9c49d8
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2025-08-13 19:41:19 +0100

    xsolla 2

--- routes/xsolla.js
+++ routes/xsolla.js
@@ -92,7 +92,7 @@ router.post('/get-token', async (req, res) => {
 
   try {
     const response = await axios.post(
-      `https://api.xsolla.com/api/v2/projects/${PROJECT_ID}/token`,
+      `https://store.xsolla.com/api/v2/project/${PROJECT_ID}/payment/token`,
       payload,
       {
         headers: {
