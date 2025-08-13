6053ca4e9cef461f25486bf7d8345738b88f5d3e
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2025-08-13 04:41:17 +0100

    xsolla

--- routes/xsolla.js
+++ routes/xsolla.js
@@ -75,11 +75,15 @@ router.post('/get-token', async (req, res) => {
     id: { value: username }
   },
   purchase: {
-    virtual_items: {
-      [sku]: 1  
-    }
+    virtual_items: [
+      {
+        sku: sku,
+        quantity: 1
+      }
+    ]
   }
 };
+
 
   console.log('[DEBUG] Payload being sent to Xsolla:', JSON.stringify(payload, null, 2));
 
