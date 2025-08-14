11be73b8c93537b96dfcf52e53d2947ef373d0c0
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2025-08-14 01:21:04 +0100

    xsolla

--- routes/xsolla.js
+++ routes/xsolla.js
@@ -22,18 +22,22 @@ router.post('/get-token', async (req, res) => {
   }
 
   try {
+    
     const payload = {
   user: {
-    id: { value: username },
+    id: { value: username }
   },
   purchase: {
-    virtual_items: { sku, quantity: 1 }
+    virtual_items: [
+      { sku: sku, amount: 1 }
+    ]
   },
   settings: {
     return_url: 'https://myme.live/shop',
     language: 'en'
   }
 };
+
 
 
     const TOKEN_URL = `https://api.xsolla.com/merchant/v2/projects/${process.env.XSOLLA_PROJECT_ID}/token`;
