0283c5f1d212e24c783d944585ac91dc3145b6be
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2025-08-14 01:51:58 +0100

    xsolla

--- routes/xsolla.js
+++ routes/xsolla.js
@@ -26,10 +26,9 @@ router.post('/get-token', async (req, res) => {
         id: { value: username }
       },
       purchase: {
-        virtual_items: {
-          sku: sku,
-          quantity: 1
-        }
+        virtual_items: [
+          { sku: sku }
+        ]
       },
       settings: {
         return_url: 'https://myme.live/shop',
