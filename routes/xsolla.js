a0de33449110762fabbf3b9708fad7e281cc9231
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2025-08-14 19:56:50 +0100

    xsolla

--- routes/xsolla.js
+++ routes/xsolla.js
@@ -42,7 +42,8 @@ router.post('/get-token', async (req, res) => {
 
     console.log('[DEBUG] Sending payload to Xsolla:', JSON.stringify(payload, null, 2));
 
-    const TOKEN_URL = `https://api.xsolla.com/merchant/v2/projects/${process.env.XSOLLA_PROJECT_ID}/token`;
+    const TOKEN_URL = `https://api.xsolla.com/merchant/v2/projects/${process.env.XSOLLA_PROJECT_ID}/payment_token`;
+
 
     const response = await axios.post(TOKEN_URL, payload, {
       auth: {
