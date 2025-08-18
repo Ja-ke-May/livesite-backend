a981308219123d195b2606a05ba09d6ade3d10da
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2025-08-18 01:27:58 +0100

    notes square

--- emails.js
+++ emails.js
@@ -69,11 +69,11 @@ async function sendThankYouEmail(user, purchaseDetails) {
     html: `
       <div style="font-family: Arial, sans-serif; background-color: #000110; color: black; padding: 30px; border-radius: 10px; max-width: 600px; margin: 0 auto; box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);">
         <h1 style="text-align: center; color: yellow; font-size: 24px; margin-bottom: 20px;">Thank You for Your Purchase!</h1>
-        <p style="font-size: 16px; color: black; line-height: 1.6;">Dear <span style="font-weight: bold;">${user.userName}</span>,</p>
-        <p style="font-size: 16px; color: black; line-height: 1.6;">Thank you for purchasing tokens on <span style="color: yellow; font-weight: bold;">MyMe.Live</span>. Your support means a lot to us!</p>
-        <p style="font-size: 16px; color: black; line-height: 1.6;">You have purchased <span style="font-weight: bold; color: yellow;">${purchaseDetails.tokens}</span> tokens for a total of <span style="font-weight: bold;">${purchaseDetails.amountSpent} ${purchaseDetails.currency}</span>.</p>
+        <p style="font-size: 16px; color: white; line-height: 1.6;">Dear <span style="font-weight: bold;">${user.userName}</span>,</p>
+        <p style="font-size: 16px; color: white; line-height: 1.6;">Thank you for purchasing tokens on <span style="color: yellow; font-weight: bold;">MyMe.Live</span>. Your support means a lot to us!</p>
+        <p style="font-size: 16px; color: white; line-height: 1.6;">You have purchased <span style="font-weight: bold; color: yellow;">${purchaseDetails.tokens}</span> tokens for a total of <span style="font-weight: bold;">${purchaseDetails.amountSpent} ${purchaseDetails.currency}</span>.</p>
 
-        <p style="font-size: 16px; color: black; line-height: 1.6;">Your tokens have been added to your account, and you can now use them to support your favorite creators.</p>
+        <p style="font-size: 16px; color: white; line-height: 1.6;">Your tokens have been added to your account, and you can now use them to support your favorite creators.</p>
 
         <div style="text-align: center; margin-top: 30px;">
           <a href="${process.env.FRONTEND_URL}/" 
@@ -82,8 +82,8 @@ async function sendThankYouEmail(user, purchaseDetails) {
           </a>
         </div>
         
-        <p style="font-size: 16px; color: black; margin-top: 30px;">Best regards,</p>
-        <p style="font-size: 16px; color: black; font-weight: bold;">MyMe Team</p>
+        <p style="font-size: 16px; color: white; margin-top: 30px;">Best regards,</p>
+        <p style="font-size: 16px; color: white; font-weight: bold;">MyMe Team</p>
         
         <div style="text-align: center; margin-top: 50px;">
           <a href="${process.env.FRONTEND_URL}" style="text-decoration: none;">
